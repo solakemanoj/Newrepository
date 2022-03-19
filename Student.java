4c2e7fcfe6ddecfb4c6b7ac27a409616daab6de1
solakemanoj <101618339+solakemanoj@users.noreply.github.com>
2022-03-19 21:47:13 +0530

    2nd change

---
+++ Student.java
@@ -0,0 +1,26 @@
+package Student;
+
+public class Student {
+
+
+		int a;
+		float b;
+		char c;
+		boolean d;
+		
+		static boolean j;
+		public static void main(String[] args) {
+			
+			int k=5; //local variable
+			float e= 12.323f;
+			Student st = new Student ();
+			System.out.println(k);
+			system.out.println(e);
+			System.out.println(st.a);
+			System.out.println(st.b);
+			System.out.println(st.c);
+			System.out.println(st.d);
+			
+		}
+
+	}
